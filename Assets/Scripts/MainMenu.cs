ca4f07c230dcbfded802d9890b0e802589aa8985
Galeaz <50804911+Galeaz@users.noreply.github.com>
2023-04-30 17:31:16 -0700

    Added seasons button
    added all buttons but only spring is interactable

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 
 public class MainMenu : MonoBehaviour
 {
+    //singleton
+    public static MainMenu MM_instance;
+    public GameObject playDemo_Button;
+    public GameObject credits_Button;
+    public GameObject quit_Button;
+    private void Awake()
+    {
+        MM_instance = this;
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameObject.SetActive(true); //activates menu
     }
 
     // Update is called once per frame
